757962b83cff1fed09b296aacca664d72a6ff1a7
Osama Shehta <osama.shehtafathi@gmail.com>
2025-03-04 09:47:33 +0200

    Register with React-Hook-Form

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <div>
-      Welcome Home!
+    <div className="flex justify-center space-x-12 mt-12">
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
     </div>
   )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import axios from "axios";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 type TFormData = {
   name: string;
@@ -10,103 +11,142 @@ type TFormData = {
   phone: string;
 };
 function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [rePassword, setRePassword] = useState('');
-  const [phone, setPhone] = useState('');
-  const [errors, setErrors] = useState<string[]>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    getValues
+  } = useForm<TFormData>();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const data: TFormData = {
-      name,
-      email,
-      password,
-      rePassword,
-      phone,
-    };
-    console.log("data before", data);
-    if(password !== rePassword){
-      setErrors(["Password and Confirm Password should be the same"]);
-      return;
-    }
-    
+  const sendData = async (data: TFormData) => {
     try {
       const res = await axios.post(
         "https://ecommerce.routemisr.com/api/v1/auth/signup",
         data
       );
       console.log("data after", data);
-      setName('');
-      setEmail('');
-      setPassword('');
-      setRePassword('');
-      setPhone('');
-      setErrors([]);
+
       console.log(res);
-    } catch (error) {
+      if(res.data.message === "success"){
+
+        toast.success("User registered successfully");
+      }
+      reset();
+    } catch (error: any) {
       console.error("Error submitting form", error);
+      toast.error(error.response?.data?.message);
     }
   };
 
   return (
     <div className={styles.formWrapper}>
       <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
+      
+      <form onSubmit={handleSubmit(sendData)}>
 
-        {errors.length > 0 && (
-          <ul>
-            {errors.map((error) => (
-              <li key={error} className="text-red-800 text-center my-2">{error}</li>
-            ))}
-          </ul>
-        )}
+
         <label htmlFor="name">User Name</label>
         <input
-          onChange={(e) => setName(  e.target.value )}
           id="name"
           type="text"
           placeholder="Username"
-          value={name}
+          {...register("name", {
+            required: "This field is required",
+            minLength: {
+              value: 5,
+              message: "Username must be at least 5 characters",
+            },
+          })}
         />
+        {errors.name && errors.name.type === "required" && (
+          <p className={styles.error}>{errors.name.message}</p>
+        )}
+        {errors.name && errors.name.type === "minLength" && (
+          <p className={styles.error}>{errors.name.message}</p>
+        )}
+
 
         <label htmlFor="email">Email</label>
         <input
           id="email"
           type="email"
           placeholder="Email"
-          onChange={(e)=> setEmail(e.target.value)}
-          value={email}
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Invalid email address",
+            },
+          })}
         />
+
+{errors.email && errors.email.type === "required" && (
+          <p className={styles.error}>{errors.email.message}</p>
+        )}
+
+        {errors.email && errors.email.type === "pattern" && (
+          <p className={styles.error}>{errors.email.message}</p>
+        ) }
+
 
         <label htmlFor="password">Password</label>
         <input
           id="password"
           type="password"
           placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
         />
+        {errors.password && errors.password.type === "required" && (
+          <p className={styles.error}>{errors.password.message}</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p className={styles.error}>{errors.password.message}</p>
+        )}
 
         <label htmlFor="rePassword">Confirm Password</label>
         <input
           id="rePassword"
           type="password"
           placeholder="Confirm Password"
-          onChange={(e)=> setRePassword(e.target.value)}
-          value={rePassword}
+          {...register("rePassword", {
+            required: "This field is required",
+            validate: (value) =>
+              value === getValues("password") || "The passwords do not match",
+          })}
         />
+        {errors.rePassword && errors.rePassword.type === "required" && (
+          <p className={styles.error}>{errors.rePassword.message}</p>
+        )}
+        {errors.rePassword && errors.rePassword.type === "validate" && (
+          <p className={styles.error}>{errors.rePassword.message}</p>
+        )}
 
         <label htmlFor="phone">Phone</label>
         <input
           id="phone"
           type="tel"
           placeholder="Phone"
-         onChange={(e)=> setPhone(e.target.value)}
-          value={phone}
+          {...register("phone", {
+            required: "This field is required",
+            pattern: {
+              value: /^(01[0-2,5])[0-9]{8}$/,
+              message: "Enter Egyptian number",
+            },
+          })}
         />
+        {errors.phone && errors.phone.type === "required" && (
+          <p className={styles.error}>{errors.phone.message}</p>
+        )}
+        {errors.phone && errors.phone.type === "pattern" && (
+          <p className={styles.error}>{errors.phone.message}</p>
+        )}
         <button type="submit" className={styles.button}>
           Register
         </button>
